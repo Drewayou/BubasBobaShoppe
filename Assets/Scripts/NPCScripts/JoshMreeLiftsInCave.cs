36b39ad265230100d4702ead1bd6bc66636a4207
Drewayou <49324678+Drewayou@users.noreply.github.com>
2024-04-18 17:20:56 -0400

    Alpha - 2.07 - Added the first NPC and a Dialogue script!
    Although the script itself isn't efficient, it works! May be better to just make custom scripts for each NPC / Scenario...

---
+++ Assets/Scripts/NPCScripts/JoshMreeLiftsInCave.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Note: This is chat interaction number 5
+public class JoshMreeLiftsInCave : MonoBehaviour
+{
+    public string greetings = "Hai, I'm Josh Mree!";
+
+    public string PlayerRespGreet = "Ello there... Are you lifting Rocks?"; 
+
+    public string Dia1 = "Yes! The local Gym doesn't have heavy enough weights so I come here.";
+
+    public string PlayerResp1 = "Well be careful out here... The fruit monsters seem to be getting tougher"; 
+
+    public string Dia2 = "Don't worry Mister! I'm good. Even moonwalked away from an Ube monster recently!"; 
+
+    public string PlayerResp2 = "Moonwalked? Like Michale Jackleson?"; 
+
+    public string Dia3 = "Yeah! I can show you how to! Hahaha.";
+
+    public string PlayerResp3 { get; set; } 
+
+    public string Dia4 = "Press 'm' on keyboard and you'll be moonwalking in no time!";
+
+    public string PlayerResp4 = "Huh... Alright, thanks Josh. I'll give that a try...";
+
+    public string Dia5 { get; set; }
+
+    public string PlayerResp5 { get; set; } 
+
+    public string Dia6 { get; set; }
+    
+    public string PlayerResp6 { get; set; } 
+
+    public string Dia7 { get; set; }
+
+    public string PlayerResp7 { get; set; } 
+
+    public string Dia8 { get; set; }
+
+    public string PlayerResp8 { get; set; } 
+
+    public string Dia9 { get; set; }
+
+    public string PlayerResp9 { get; set; } 
+
+    public string Dia10 { get; set; }
+
+    public string PlayerResp10 { get; set; } 
+
+    public string Goodbye = "See ya around!";
+
+    public string PlayerRespBye { get; set; } 
+}
---
+++ Assets/Scripts/NPCScripts/NpcJsonDiaStructure.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcDiaStructure : MonoBehaviour
+{
+    public string Greetings { get; set; }
+
+    public string PlayerRespGreet { get; set; } 
+
+    public string Dia1 { get; set; }
+
+    public string PlayerResp1 { get; set; } 
+
+    public string Dia2 { get; set; }
+
+    public string PlayerResp2 { get; set; } 
+
+    public string Dia3 { get; set; }
+
+    public string PlayerResp3 { get; set; } 
+
+    public string Dia4 { get; set; }
+
+    public string PlayerResp4 { get; set; } 
+
+    public string Dia5 { get; set; }
+
+    public string PlayerResp5 { get; set; } 
+
+    public string Dia6 { get; set; }
+    
+    public string PlayerResp6 { get; set; } 
+
+    public string Dia7 { get; set; }
+
+    public string PlayerResp7 { get; set; } 
+
+    public string Dia8 { get; set; }
+
+    public string PlayerResp8 { get; set; } 
+
+    public string Dia9 { get; set; }
+
+    public string PlayerResp9 { get; set; } 
+
+    public string Dia10 { get; set; }
+
+    public string PlayerResp10 { get; set; } 
+
+    public string Goodbye { get; set;}
+
+    public string PlayerRespBye { get; set; } 
+}
