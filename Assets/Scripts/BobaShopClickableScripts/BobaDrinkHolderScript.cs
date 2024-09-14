302fa4408a72a0ac688bf0e3b5d0e01350e56c71
Drewayou <49324678+Drewayou@users.noreply.github.com>
2024-09-14 19:40:00 -0400

    Beta - 1.30.2 - Rough toppings into empty cups work!
    Managed to find a quick & easy way to have toppings be placed into empty boba cups! Next is to limit toppings to 1 per cup (for now), and have the mixer machine acknowledge toppings are in the empty cup placed inside of it!

--- Assets/Scripts/BobaShopClickableScripts/BobaDrinkHolderScript.cs
+++ Assets/Scripts/BobaShopClickableScripts/BobaDrinkHolderScript.cs
@@ -10,6 +10,21 @@ public class BobaDrinkHolderScript : MonoBehaviour
     [SerializeField]
     GameObject itemInHandInventory;
 
+    //Boba toppings in cup prefab.
+    [SerializeField]
+    [Tooltip("Drag the \"Boba topping in cup prefab\" here!")]
+    GameObject bobaToppingsInCupPrefab;
+
+    //Empty cup prefab.
+    [SerializeField]
+    [Tooltip("Drag the \"Empty cup prefab\" here!")]
+    GameObject emptyCup;
+
+    //Empty cup prefab.
+    [SerializeField]
+    [Tooltip("Drag the \"bobaLadle prefab\" here!")]
+    GameObject bobaladle;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -36,6 +51,45 @@ public class BobaDrinkHolderScript : MonoBehaviour
             itemInHandInventory.transform.GetChild(0).transform.position = new Vector3(0f,-.75f,0f);
             itemInHandInventory.transform.GetChild(0).transform.localScale = new Vector3(3f,3f,3f);
         }
+
+        //Check if hand inventory has a ladle with toppings, and if the cup holder has an EMPTY cup, then move the correct toppings to the cup inside the cup holder.
+        if(itemInHandInventory.transform.childCount != 0 && itemInHandInventory.transform.GetChild(0).gameObject.name == bobaladle.name && gameObject.transform.childCount != 0 && gameObject.transform.GetChild(0).name == emptyCup.name + "(Clone)"){
+            //Check what topping the ladle holds to change it's settings.
+
+            //FIXME: Check if ladle has only 1 TOPPING (If more, cause a spill).
+            if(itemInHandInventory.transform.GetChild(0).GetComponent<BobaLadelScript>().GetAmmountOfIngredientsInLadle() == 1){
+            //Check if the BOBA topping is in the ladle.
+                if(itemInHandInventory.transform.GetChild(0).transform.GetChild(1).gameObject.activeSelf){
+                    //Put boba toppings prefab into the cup.
+                    Instantiate(bobaToppingsInCupPrefab, gameObject.transform.GetChild(0).gameObject.transform.position, Quaternion.identity, gameObject.transform.GetChild(0).gameObject.transform);
+                    //Reset the ladle active topping to empty, and reduce the items in the ladle to 0.
+                    itemInHandInventory.transform.GetChild(0).transform.GetChild(1).gameObject.SetActive(false);
+                    itemInHandInventory.transform.GetChild(0).transform.GetChild(0).gameObject.SetActive(true);
+                    itemInHandInventory.transform.GetChild(0).GetComponent<BobaLadelScript>().SetAmmountOfIngredientsInLadle(0);
+                }
+                //Check if the LYCHEE toppings is in the ladle.
+                if(itemInHandInventory.transform.GetChild(0).transform.GetChild(2).gameObject.activeSelf){
+                    //Put boba toppings prefab into the cup.
+                        //FIXME: Instantiate(lycheeToppingsPrefab, new Vector3(0,0,0), Quaternion.identity, emptyCup.transform);
+                    //Reset the ladle active topping to empty, and reduce the items in the ladle to 0.
+                    itemInHandInventory.transform.GetChild(0).transform.GetChild(2).gameObject.SetActive(false);
+                    itemInHandInventory.transform.GetChild(0).transform.GetChild(0).gameObject.SetActive(true);
+                    itemInHandInventory.transform.GetChild(0).GetComponent<BobaLadelScript>().SetAmmountOfIngredientsInLadle(0);
+                }
+                //Check if the REDBEAN toppings is in the ladle.
+                if(itemInHandInventory.transform.GetChild(0).transform.GetChild(3).gameObject.activeSelf){
+                    //Put boba toppings prefab into the cup.
+                        //FIXME: Instantiate(redBeanToppingsPrefab, new Vector3(0,0,0), Quaternion.identity, emptyCup.transform);
+                    //Reset the ladle active topping to empty, and reduce the items in the ladle to 0.
+                    itemInHandInventory.transform.GetChild(0).transform.GetChild(3).gameObject.SetActive(false);
+                    itemInHandInventory.transform.GetChild(0).transform.GetChild(0).gameObject.SetActive(true);
+                    itemInHandInventory.transform.GetChild(0).GetComponent<BobaLadelScript>().SetAmmountOfIngredientsInLadle(0);
+                }
+            }else{
+                //FIXME: Cause an overflow spill if there is more than 1 topping in the ladle (You loose the cup and all the toppings!).
+            }
+        }
+                
         
         //Player interaction if they choose to place something other than a boba drink into the cup holder.
         if(itemInHandInventory.transform.childCount != 0 && itemInHandInventory.transform.GetChild(0).gameObject.tag != "BobaDrink"){
