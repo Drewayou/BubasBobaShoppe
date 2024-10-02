199344059502d9b56c47e378819bc51dbf841d8b
Drewayou <49324678+Drewayou@users.noreply.github.com>
2024-10-02 01:18:31 -0400

    Beta - 1.31.6 - Basic UID script.
    Added a UID script to further work on.

---
+++ Assets/Scripts/BobaShopClickableScripts/BobaCupUIDSettingsScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BobaCupUIDSettingsScript : MonoBehaviour
+{
+    // String for specific UID of this drink.
+    public string drinkUID;
+
+    // Check if the drink has any tea overlays.
+    public bool hasTeaOverlay;
+
+    // Save what type of drink overlay this drink has.
+    public int teaOverlaySelectionNumber;
+
+    // Check if the drink has any additional overlays.
+    public bool hasAdditionalFlavorOverlay;
+
+    // Save what type of drink overlay this drink has.
+    public int additionalOverlaySelectionNumber;
+
+    // Check if the drink has any additional overlays.
+    public bool hasToppingsOverlay;
+
+    // Save what type of drink overlay this drink has.
+    public int toppingInTheDrink;
+
+}
