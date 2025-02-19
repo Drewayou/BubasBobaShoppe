316414eba2b60b183ed5e7b7f64d747f383bb730
Drewayou <49324678+Drewayou@users.noreply.github.com>
2025-02-18 22:45:41 -0500

    Beta - 1.57.5 - Working on customer script

--- Assets/customerdrinkscript.cs
+++ Assets/customerdrinkscript.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CustomerDrinkScript : MonoBehaviour
@@ -5,6 +6,9 @@ public class CustomerDrinkScript : MonoBehaviour
     // The GameManager script pulled from the game manager object.
     // Usefull to determine the player stats for what drinks they unlocked.
     GameManagerScript thisGamesOverallInstanceScript;
+
+    // The List of favorite drinks this character wants.
+    List<GameObject> characterFavoriteBobaDrinks = new List<GameObject>();
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
