ddd7e2e391a58a9021b2e5e0a2f74ef1303bee8a
Drewayou <49324678+Drewayou@users.noreply.github.com>
2025-02-18 19:06:20 -0500

    Beta - 1.57.4 - Working on customer script

--- Assets/customerdrinkscript.cs
+++ Assets/customerdrinkscript.cs
@@ -3,12 +3,13 @@ using UnityEngine;
 public class CustomerDrinkScript : MonoBehaviour
 {
     // The GameManager script pulled from the game manager object.
+    // Usefull to determine the player stats for what drinks they unlocked.
     GameManagerScript thisGamesOverallInstanceScript;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        thisGamesOverallInstanceScript = GameObject.Find("GameManagerObject").GetComponent<GameManagerScript>();
     }
 
     // Update is called once per frame
